8c0317d9d566b3da24b7fd73fe12f0320d463390
Manavsharma7017 <manavsharma7017@gmail.com>
2024-10-09 00:36:16 +0530

    link list

---
+++ simple-link-list.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node *next;
+};
+int main(){
+    Node *newnode=new Node();
+    newnode->data=3;
+    cout<<newnode->data<<endl;
+}
