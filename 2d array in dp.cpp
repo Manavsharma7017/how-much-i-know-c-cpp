6b718d1b34fb611801c548b7e90de0bc42849ee9
Manavsharma7017 <manavsharma7017@gmail.com>
2024-10-09 00:03:02 +0530

    link list

--- 2d array in dp.cpp
+++ 2d array in dp.cpp
@@ -1,37 +1,36 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    int n,m;
-    cin>>n>>m;
-    int **arr=new int*[n];
-    for (size_t i = 0; i <n; i++)
-    {
-        arr[i]=new int[m];
-    }
-    cout<<endl;
-    for (size_t i = 0; i < n; i++)
-    {
-        for (size_t j = 0; j <m; j++)
-        {
-           cin>>arr[i][m];
-        }
-        
-    }
-    int sum =0;
-      for (size_t i = 0; i < n; i++)
-    {
-        for (size_t j = 0; j <m; j++)
-        {
-           sum=sum+arr[i][m];
-        }
-        
-    }
-  cout<<sum<<endl;
+int main()
+{
+  int n, m;
+  cin >> n >> m;
+  int **arr = new int *[n];
   for (size_t i = 0; i < n; i++)
   {
-    delete []arr[i];
+    arr[i] = new int[m];
   }
-  delete []arr;
-  
-    return 0;
+  cout << endl;
+  for (size_t i = 0; i < n; i++)
+  {
+    for (size_t j = 0; j < m; j++)
+    {
+      cin >> arr[i][m];
+    }
+  }
+  int sum = 0;
+  for (size_t i = 0; i < n; i++)
+  {
+    for (size_t j = 0; j < m; j++)
+    {
+      sum = sum + arr[i][m];
+    }
+  }
+  cout << sum << endl;
+  for (size_t i = 0; i < n; i++)
+  {
+    delete[] arr[i];
+  }
+  delete[] arr;
+
+  return 0;
 }
